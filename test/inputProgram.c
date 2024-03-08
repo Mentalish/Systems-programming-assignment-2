92c4a69e2c8860eeb48224e35d04405a20959d86
Erik Jimenez <ej.erik.jimenez@gmail.com>
2024-03-07 19:48:25 -0600

    Added test files

---
+++ test/inputProgram.c
@@ -0,0 +1,21 @@
+/**
+author:     Some Student
+**/
+#include <stdio.h>
+
+int main() {
+    int numberIn;
+
+    printf("Enter a Number: ");
+
+    scanf("%d", &numberIn);  
+
+    if ( numberIn > 10 ) {
+    //    add  two
+        return numberIn  +  2;
+    } else   if    (numberIn<5){
+    //    subtract two   
+        return  numberIn - 2;
+
+    return  numberIn*2;
+}
